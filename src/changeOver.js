84fbcd692d83bf62726e7942c2f9ef318198c11a
Valen <v.lunaklick@gmail.com>
2021-12-25 17:47:36 -0300

    Better UI and responsive design

--- src/changeOver.js
+++ src/changeOver.js
@@ -1,51 +1,80 @@
-export default function(data){
-    let countryTit = document.getElementById('country');
-    let tempTit = document.getElementById('temperature');
-    let descSky = document.getElementById('descSky');
-    let wind = document.getElementById('wind');
-    let humidity = document.getElementById('humidity');
-    let miniT = document.getElementById('minT');
-    let maxT = document.getElementById('maxT');
-    let hour = document.getElementById('hour');
-    let fecha = document.getElementById('fecha');
+export default function (data) {
+	let countryTit = document.getElementById('country')
+	let tempTit = document.getElementById('temperature')
+	let descSky = document.getElementById('descSky')
+	let wind = document.getElementById('wind')
+	let humidity = document.getElementById('humidity')
+	let miniT = document.getElementById('minT')
+	let maxT = document.getElementById('maxT')
+	let hour = document.getElementById('hour')
+	let fecha = document.getElementById('fecha')
 
-    let body = document.querySelector('body');
+	let grapper = document.getElementById('grapper')
 
-    countryTit.innerHTML = `${data.name}, ${mayus((data.sys.country).toLowerCase())}`;
-    tempTit.innerHTML = `${Math.round((data.main.temp - 273.15)*10)/10} C°`;
-    descSky.innerHTML = `${mayus(data.weather[0].description)}`;
-    wind.innerHTML = `${data.wind.speed}k/m`;
-    humidity.innerHTML = `${data.main.humidity}%`;
-    miniT.innerHTML = `<strong>Min. today:</strong> ${Math.round((data.main.temp_min - 273.15)*10)/10} C°`;
-    maxT.innerHTML = `<strong>Max. today:</strong> ${Math.round((data.main.temp_max - 273.15)*10)/10} C°`;
-    hour.innerHTML = `${(getLocalTime(data.timezone)).slice(10)}`;
-    fecha.innerHTML = `${months[getLocalTime(data.timezone).slice(0,1)]}, ${days[getLocalTime(data.timezone).slice(1,2)]}`;
+	countryTit.innerHTML = `${data.name}, ${mayus(
+		data.sys.country.toLowerCase()
+	)}`
+	tempTit.innerHTML = `${Math.round((data.main.temp - 273.15) * 10) / 10} C°`
+	descSky.innerHTML = `${mayus(data.weather[0].description)}`
+	wind.innerHTML = `${data.wind.speed}k/m`
+	humidity.innerHTML = `${data.main.humidity}%`
+	miniT.innerHTML = `<strong>Min. today:</strong> ${
+		Math.round((data.main.temp_min - 273.15) * 10) / 10
+	} C°`
+	maxT.innerHTML = `<strong>Max. today:</strong> ${
+		Math.round((data.main.temp_max - 273.15) * 10) / 10
+	} C°`
+	hour.innerHTML = `${getLocalTime(data.timezone).slice(10)}`
+	fecha.innerHTML = `${months[getLocalTime(data.timezone).slice(0, 1)]}, ${
+		days[getLocalTime(data.timezone).slice(1, 2)]
+	}`
 
-    if ((getLocalTime(data.timezone)).slice(10,12) > 18){
-        body.style.backgroundImage = `url('images/anochecer.jpg')`;
-    } else if ((getLocalTime(data.timezone)).slice(10,12) > 12){
-        body.style.backgroundImage = `url('images/mediodia.jpg')`;
-    } else {
-        body.style.backgroundImage = `url('images/amanecer.jpg')`;
-    }
+	if (parseInt(getLocalTime(data.timezone).slice(10, 13)) >= 18) {
+		grapper.style.backgroundImage = `url('images/noche.jpg')`
+	} else if (parseInt(getLocalTime(data.timezone).slice(10, 13)) >= 12) {
+		grapper.style.backgroundImage = `url('images/mediodia.jpg')`
+	} else {
+		grapper.style.backgroundImage = `url('images/mañana.jpg')`
+	}
 }
 
-function mayus(cont){
-    let ma = cont.slice(0,1)
-    ma = ma.toUpperCase()
-    let res = cont.slice(1)
-    return ma + res
+function mayus(cont) {
+	let ma = cont.slice(0, 1)
+	ma = ma.toUpperCase()
+	let res = cont.slice(1)
+	return ma + res
 }
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+]
+const days = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday',
+]
 
 function getLocalTime(data) {
-    let date = new Date();
-    let time = date.getTime();
-    let localOffset = date.getTimezoneOffset() * 60000;
-    let utc = time + localOffset;
-    let localTime = utc + 1000 * data;
-    let localTimeDate = new Date(localTime);
-    return localTimeDate.toLocaleString();
-  }
+	let date = new Date()
+	let time = date.getTime()
+	let localOffset = date.getTimezoneOffset() * 60000
+	let utc = time + localOffset
+	let localTime = utc + 1000 * data
+	let localTimeDate = new Date(localTime)
+	return localTimeDate.toLocaleString()
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
-import changeOver from './changeOver.js';
-import './style.css';
+import changeOver from './changeOver.js'
+import './style.css'
 
-let searcher = document.getElementById('searcher');
+let searcher = document.getElementById('searcher')
+let labelSearcher = document.getElementById('labelSearcher')
+
+searcher.value = ''
 
 async function searchWeather(country) {
-    try{
-        const weatherG = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${country}&APPID=2fa59fcee881881732ea84b56fe7e565`,{mode:"cors"});
-        const valores = await weatherG.json();
-        changeOver(valores);
-    } catch {
-        alert("That location doesn't exist")
-    }
+	try {
+		const weatherG = await fetch(
+			`http://api.openweathermap.org/data/2.5/weather?q=${country}&APPID=2fa59fcee881881732ea84b56fe7e565`,
+			{ mode: 'cors' }
+		)
+		const valores = await weatherG.json()
+		changeOver(valores)
+	} catch {
+		alert("That location doesn't exist")
+	}
 }
 
-searchWeather("Argentina");
+searchWeather('Argentina')
 
-searcher.addEventListener('change',function(e){
-    searchWeather(e.originalTarget.value);
+searcher.addEventListener('change', function (e) {
+	searchWeather(e.originalTarget.value)
+})
+
+searcher.addEventListener('focus', function (e) {
+	labelSearcher.classList.add('moverLugar')
+})
+
+searcher.addEventListener('focusout', function (e) {
+	if (searcher.value == '') {
+		labelSearcher.classList.remove('moverLugar')
+	}
 })
